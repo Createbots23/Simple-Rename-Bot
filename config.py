6c3c1598cfbb63ea614c41b9c4ad45bc3aed9efc
Createbots23 <bots14237@gmail.com>
2024-06-19 19:11:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", "21740783"))
 API_HASH = environ.get("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", "6299192020 5326198063"))          
+ADMIN = int(environ.get("ADMIN", "6299192020"))          
 CAPTION = environ.get("CAPTION", "Anime_Warrior_Tamil")
 
 # for thumbnail ( back end is MrMKN brain 😉)
