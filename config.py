5321266968c4e5670a8247c98e4d183304258c6e
Createbots23 <bots14237@gmail.com>
2024-06-19 19:03:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "21740783"))
+API_HASH = environ.get("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+ADMIN = int(environ.get("ADMIN", "6299192020 5326198063"))          
+CAPTION = environ.get("CAPTION", "Anime_Warrior_Tamil")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
